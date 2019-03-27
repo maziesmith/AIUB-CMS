144c1fa59eea5634a3cc94809c96fd8911e89f7a
Saqibur Rahman <saqibur.rahman98@gmail.com>
2019-03-27 23:27:30 +0600

    Fix ID validation errors

--- AIUB-CMS/IDValidationModule/IDValidator.cs
+++ AIUB-CMS/IDValidationModule/IDValidator.cs
@@ -24,7 +24,9 @@ namespace AIUB_CMS
                 if (splitID[0].Length == 2 || splitID[0].Length == 4)
                     if (splitID[1].Length == 5)
                         if (splitID[2].Length == 1)
-                            if (int.Parse(splitID[2]) <= 3)
+                            if (int.Parse(splitID[2]) <= 2 && splitID[0].Length == 4 && int.Parse(splitID[2]) != 0)
+                                return true;
+                            else if (int.Parse(splitID[2]) <= 3 && splitID[0].Length == 2 && int.Parse(splitID[2]) != 0)
                                 return true;
             }
 
