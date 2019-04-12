b22a219a249c13dbce90efb8442e74b10689d674
Saqibur Rahman <saqibur.rahman98@gmail.com>
2019-04-12 21:56:07 +0600

    Restart project with organization

---
+++ AIUB_CMS.Handler/Runner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AIUB_CMS.Login.Interface;
+
+namespace AIUB_CMS.Handler
+{
+    static class Runner
+    {
+        static void Main()
+        {
+            LoginFormInitializer.InitializeLoginForm();
+        }
+    }
+}
---
+++ AIUB_CMS.Login/Interface/LoginFormInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIUB_CMS.Login.Interface
+{
+    public static class LoginFormInitializer
+    {
+        public static void InitializeLoginForm()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginInterface());
+        }
+    }
+}
