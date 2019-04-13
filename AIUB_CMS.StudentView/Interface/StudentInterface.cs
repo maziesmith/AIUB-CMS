e95aa4e94823882ecd5f1793ed138a9c0fea6556
Saqibur Rahman <saqibur.rahman98@gmail.com>
2019-04-13 10:37:08 +0600

    Student design and logic

--- AIUB_CMS.StudentView/Interface/StudentInterface.cs
+++ AIUB_CMS.StudentView/Interface/StudentInterface.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,23 +9,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Google.GData.Calendar;
+using AIUB_CMS.StudentView.Data;
 
 namespace AIUB_CMS.StudentView.Interface
 {
     public partial class StudentInterface : MetroFramework.Forms.MetroForm
     {
-        string name;
-        string id;
-        string CGPA;
-        string credit;
-        string program;
-        string department;
-        string core;
-        string father;
-        string mother;
-        string phone;
-        string email;
-
         public StudentInterface()
         {
             // Do nothing.
@@ -33,14 +23,22 @@ namespace AIUB_CMS.StudentView.Interface
         public StudentInterface(string id)
         {
             InitializeComponent();
-            
 
+            StudentDataHandler studentData = new StudentDataHandler(id);
 
-        }
-
-        private GetStudentInfo(string id)
-        {
-
+            this.labelProgramAns.Text = studentData.GetProgram();
+            this.labelPhoneAns.Text = studentData.GetPhone();
+            this.labelNameAns.Text = studentData.GetName();
+            this.labelMotherAns.Text = studentData.GetMother();
+            this.labelIDAns.Text = studentData.GetID();
+            this.labelGenderAns.Text = studentData.GetGender().ToString();
+            this.labelFatherAns.Text = studentData.GetFather();
+            this.labelEmailAns.Text = studentData.GetEmail();
+            this.labelDOBAns.Text = studentData.GetDOB();
+            this.labelDepartmentAns.Text = studentData.GetDepartment();
+            this.labelCreditsAns.Text = studentData.GetCredit().ToString();
+            this.labelCGPAAns.Text = studentData.GetCGPA().ToString();
+            this.labelBloodGroupAns.Text = studentData.GetBloodGroup().ToString();
         }
     }
 }
---
+++ AIUB_CMS.StudentView/Logic/StudentInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIUB_CMS.StudentView.Logic
+{
+    class StudentInfo
+    {
+        string name;
+        string id;
+        string CGPA;
+        string credit;
+        string program;
+        string department;
+        string core;
+        string father;
+        string mother;
+        string phone;
+        string email;
+
+        public StudentInfo()
+        {
+            // Do nothing.
+        }
+
+        public void GetStudentInfo()
+        {
+            
+        }
+    }
+}
